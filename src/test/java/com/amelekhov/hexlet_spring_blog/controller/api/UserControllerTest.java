d71489f01a14244720892dce3b8a2df1e9d83e56
leshasmp <leshasmp1989@mail.ru>
2026-04-01 01:56:22 +0500

    Add WebMvcTest

--- src/test/java/com/amelekhov/hexlet_spring_blog/controller/api/UserControllerTest.java
+++ src/test/java/com/amelekhov/hexlet_spring_blog/controller/api/UserControllerTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
@@ -24,6 +25,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @AutoConfigureMockMvc
+@WebMvcTest(UserController.class)
 @ActiveProfiles("test")
 public class UserControllerTest {
 
