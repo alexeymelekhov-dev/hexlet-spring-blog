ab09f3f623bc6d3ca928cb293fa42b2268b25182
leshasmp <leshasmp1989@mail.ru>
2026-04-01 01:15:37 +0500

    Update ActiveProfiles test

--- src/test/java/com/amelekhov/hexlet_spring_blog/HexletSpringBlogApplicationTests.java
+++ src/test/java/com/amelekhov/hexlet_spring_blog/HexletSpringBlogApplicationTests.java
@@ -2,8 +2,10 @@ package com.amelekhov.hexlet_spring_blog;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class HexletSpringBlogApplicationTests {
 
 	@Test
--- src/test/java/com/amelekhov/hexlet_spring_blog/controller/api/PostControllerTest.java
+++ src/test/java/com/amelekhov/hexlet_spring_blog/controller/api/PostControllerTest.java
@@ -1,4 +1,11 @@
 package com.amelekhov.hexlet_spring_blog.controller.api;
 
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
 public class PostControllerTest {
 }
