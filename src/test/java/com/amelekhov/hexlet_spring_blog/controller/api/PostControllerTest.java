1bdd7a7a48d6d522e6c75455f6d9df651be08401
leshasmp <leshasmp1989@mail.ru>
2026-04-01 01:36:47 +0500

    Add WebEnvironment.MOCK

--- src/test/java/com/amelekhov/hexlet_spring_blog/controller/api/PostControllerTest.java
+++ src/test/java/com/amelekhov/hexlet_spring_blog/controller/api/PostControllerTest.java
@@ -4,7 +4,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
 public class PostControllerTest {
--- src/test/java/com/amelekhov/hexlet_spring_blog/controller/api/UserControllerTest.java
+++ src/test/java/com/amelekhov/hexlet_spring_blog/controller/api/UserControllerTest.java
@@ -22,7 +22,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
 public class UserControllerTest {
